9c924efaa2ace55ace5929b427f10f38b8e8e58a
Sequoyah <quoyahhs@hotmail.com>
2024-02-14 23:54:14 -0400

    resources/views/about.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+ <h2>Sequoyah Simmons</h2>
+ <p>I am a 23 year old educator, who teaches Information Technology and Spanish at a primary level. After completing this degree I hope to become well versed in this field and become a well rounded individual. My Favourite colour is blue </p>
+@endsection 
